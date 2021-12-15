7b8e9304286363e9c51cc1133a7bd0ac1dd76101
Ramy Ezzat <ramymagdy.rm@gmail.com>
2021-12-15 06:27:43 +0200

    Update webpack.config.js
    disable webpack watching

--- webpack.config.js
+++ webpack.config.js
@@ -27,5 +27,5 @@ module.exports = {
   // Optional and for development only. This provides the ability to
   // map the built code back to the original source format when debugging.
   devtool: false,
-  watch: true
+  watch: false
 };
